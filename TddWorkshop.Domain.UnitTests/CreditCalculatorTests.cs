a343954fc366104b4761ec4455a5d51e98c4e78e
yers95 <yers95@gmail.com>
2022-07-16 15:36:36 +0600

    step 2 finished

--- TddWorkshop.Domain.UnitTests/CreditCalculatorTests.cs
+++ TddWorkshop.Domain.UnitTests/CreditCalculatorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using AutoFixture.Xunit2;
@@ -15,11 +16,54 @@ namespace TddWorkshop.Domain.Tests;
 public class CreditCalculatorTests
 {
     [Theory, ClassData(typeof(CreditCalculatorTestData))]
-    public void Calculate_IsApproved_PointsCalculatedCorrectly(CalculateCreditRequest request, bool hasCriminalRecord,
+    public void Calculate_PointsCalculatedCorrectly(CalculateCreditRequest request, bool hasCriminalRecord,
         int points)
     {
         var res = CreditCalculator.Calculate(request, hasCriminalRecord);
         Assert.Equal(points, res.Points);
+    }
+
+    [Theory, AutoData]
+    public void Calculate_Autofixture_PercentsCalculatedCorrectly(CalculateCreditRequest request, bool hasCriminalRecord)
+    {
+        var res = CreditCalculator.Calculate(request, hasCriminalRecord);
+        Assert.Equal(res.Points.ToInterestRate(), res.InterestRate);
+    }
+
+    [Fact]
+    public void Calculate_WrongEnumEmployment_ThrowsArgumentOutOfRangeException()
+    {
+        var empl = (Employment)(-1);
+        var faker = new Faker();
+        var request = new CalculateCreditRequest(
+            new PersonalInfo(33, faker.Person.FirstName, faker.Person.LastName),
+            new CreditInfo(ConsumerCredit, 2000000, Deposit.Guarantor, empl, false),
+            new PassportInfo("1234", "123456", faker.Date.Past(), faker.Company.CompanyName())
+        );
+
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            CreditCalculator.Calculate(request, false)
+        );
+    }
+
+    [Property(Arbitrary = new[] {typeof(PostiveArbitraries)})]
+    public bool Calculate_FsCheck_PercentsCalculatedCorrectly(CalculateCreditRequest request, bool hasCriminalRecord)
+    {
+        var res = CreditCalculator.Calculate(request, hasCriminalRecord);
+        return res.Points.ToInterestRate() == res.InterestRate;
+    }
+
+    [Property(Arbitrary = new[] { typeof(PostiveArbitraries) })]
+    public bool Response_FsCheck_PercentsCalculatedCorrectly(CalculateCreditResponse response)
+    {
+        return response.Points.ToInterestRate() == response.InterestRate;
+    }
+
+    [Property(Arbitrary = new[] { typeof(PostiveArbitraries) })]
+    public bool Response_FsCheck_Greater80_IsSatisfied(CalculateCreditResponse response)
+    {
+        return response.IsApproved == response.Points > 80;
     }
 }
 
